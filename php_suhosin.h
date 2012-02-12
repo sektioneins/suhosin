80c4e2f81d55fcd8e60c3ea3100275b134d5e198
Stefan Esser <stefan@nopiracy.de>
2012-02-12 10:41:10 +0100

    Forgot this

--- php_suhosin.h
+++ php_suhosin.h
@@ -22,7 +22,7 @@
 #ifndef PHP_SUHOSIN_H
 #define PHP_SUHOSIN_H
 
-#define SUHOSIN_EXT_VERSION  "0.9.33"
+#define SUHOSIN_EXT_VERSION  "0.9.34-dev"
 
 /*#define SUHOSIN_DEBUG*/
 #define SUHOSIN_LOG "/tmp/suhosin_log.txt"
